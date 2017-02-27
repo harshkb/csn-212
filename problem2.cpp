7ba5bfb430cf163e8bbe9f26f5b6d174e653ab67
harshkb <hkbansal1997@gmail.com>
2017-02-27 21:57:35 +0530

    Update problem2.cpp

--- problem2.cpp
+++ problem2.cpp
@@ -3,12 +3,12 @@ using namespace std;
 int main()
 {
 
-int width,height;              // width and height of the path
+int width,height;              // width and height from one corner (0,0) to last corner (width,height)
 cin>>height>>width;
 int n;                        // n denotes number of blocked paths
 cin>>n;
 int i=0;
-char blocked[width+1][height+1];       // to store the value of blocked paths
+char blocked[width+1][height+1];       // matrix to denote blocked paths
 for(int x = 0; x<=width; x++)
 	{
 	for(int y = 0; y<=height; y++)
@@ -39,16 +39,25 @@ for(int x = 0; x<=width; x++)            //initially all ways are zero
 	}
 	}
 
-ways[0][0] = 1;                             // initially ways when only on [0,0] is 1
+ways[0][0] = 1;                             // initially ways when only upto [0,0] is 1
 	for(int x = 0; x<=width; x++)
 	{
 	for(int y = 0; y<=height; y++)
 	{
-		if(blocked[x-1][y]!='X'&&x>=1)      // if along x-axis is not blocked then add it in number of ways
-		ways[x][y]=ways[x][y]+ways[x-1][y];
-		if(blocked[x][y-1]!='Y'&&y>=1)      // if along y-axis is not blocked then add it in number of ways
-		ways[x][y]=ways[x][y]+ways[x][y-1];
+		if(blocked[x-1][y]!='X'&&x>=1)      // if previous block along x-axis is not blocked along x-direction 
+		ways[x][y]=ways[x][y]+ways[x-1][y]; //then add it in number of ways
+		if(blocked[x][y-1]!='Y'&&y>=1)      // if previous block along y-axis is not blocked  along y-direction 
+		ways[x][y]=ways[x][y]+ways[x][y-1]; // then add it in number of ways
 	}
 	}
-	cout<< ways[width][height]<<endl;           //total number of ways is in [width,height]
+	cout<< ways[width][height]<<endl;           //total number of ways is upto  [width,height]
 }
+// Input
+// width of the corner from 0
+// height of the corner from 0
+// number of blocked path 
+// (a,b,c,d) co-ordinates of blocked path
+
+
+//Output
+// give number of ways excluding block paths to reach at corner from (0,0)
